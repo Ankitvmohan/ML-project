999ab90f5c3a990116390fce1ecb6a5ee25a552a
Ankitvmohan <ankitvmohan0401@gmail.com>
2026-09-27 00:08:27 +0530

    Updated

--- src/pipelines/predict_pipeline.py
+++ src/pipelines/predict_pipeline.py
@@ -15,13 +15,11 @@ class PredictPipeline:
             model = load_object(file_path = model_path)
             preprocessor = load_object(file_path = preprocessor_path)
             data_scaled = preprocessor.transform(features)
-            preds = model.predict((data_scaled))
+            preds = model.predict(data_scaled)
             return preds
         
         except Exception as e:
             raise CustomException(e,sys)
-    
-    
     
 class CustomData:
     def __init__(self, 
